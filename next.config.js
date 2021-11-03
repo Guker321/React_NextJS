a752417b778253cf9ed66287a6fcf7e6318bfb38
Oleg Vashchan <guker168@gmail.com>
2021-11-03 12:44:15 +0300

    Раздел 7 добавили настройки для нового Next 11 версии изменили next.config.js svg.d.ts.tsconfig.json но автозаполнение для svg компонентов я ставить не стал поэтому работать они не будут

--- next.config.js
+++ next.config.js
@@ -1,35 +1,21 @@
 module.exports = {
-  webpack(config, options) {
-    config.module.rules.push({
-      test: /\.svg?$/,
+	images: {
+		domains: ['courses-top.ru']
+	},
+	webpack(config, options) {
+		config.module.rules.push({
+			loader: '@svgr/webpack',
+			options: {
+				prettier: false,
+				svgo: true,
+				svgoConfig: {
+					plugins: [{ removeViewBox: false }],
+				},
+				titleProp: true,
+			},
+			test: /\.svg$/,
+		});
 
-      oneOf: [
-        {
-          use: [
-            {
-              loader: '@svgr/webpack',
-
-              options: {
-                prettier: false,
-
-                svgo: true,
-
-                svgoConfig: {
-                  plugins: [{ removeViewBox: false }],
-                },
-
-                titleProp: true,
-              },
-            },
-          ],
-
-          issuer: {
-            and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
-          },
-        },
-      ],
-    });
-
-    return config;
-  },
+		return config;
+	},
 };
