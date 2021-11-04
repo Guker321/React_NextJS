2ed5ccc615dfe87153944112ae467eef0219f424
Oleg Vashchan <guker168@gmail.com>
2021-11-04 11:02:34 +0300

    Раздел 8 Написана 1 часть рейтинга звезды установил и настроил по видео работу с SVG файлами но автозаполнение не работает

--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,4 @@ export * from './Htag/Htag'; // Здесь мы укоротили путь им
 export * from './Button/Button';
 export * from './P/P';
 export * from './Tag/Tag';
+export * from './Rating/Rating';
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Htag, P, Tag } from '../components';
+import { Button, Htag, P, Rating, Tag } from '../components';
 
 export default function Home(): JSX.Element {
   return (
@@ -14,27 +14,20 @@ export default function Home(): JSX.Element {
       <P size='s'>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, minima!
         Quasi nobis, sint sed nulla numquam laudantium optio earum doloremque?
-        Suscipit beatae ullam laboriosam architecto pariatur atque repudiandae
-        sit asperiores.
       </P>
       <P size='m'>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, minima!
         Quasi nobis, sint sed nulla numquam laudantium optio earum doloremque?
-        Suscipit beatae ullam laboriosam architecto pariatur atque repudiandae
-        sit asperiores.
       </P>
       <P size='l'>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, minima!
         Quasi nobis, sint sed nulla numquam laudantium optio earum doloremque?
-        Suscipit beatae ullam laboriosam architecto pariatur atque repudiandae
-        sit asperiores.
       </P>
-      <Tag color='red' size='s'>HH Ru</Tag>
-      <Tag color='grey' size='m'>grey</Tag>
       <Tag color='green'>green</Tag>
       <Tag color='primary'>primary</Tag>
       <Tag color='ghost'>ghost</Tag>
       <Tag>ghost standart</Tag>
+      <Rating rating={4} />
     </>
   );
 }
