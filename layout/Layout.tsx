292eff9d08beec896713a0f29fe9dde4e28bdc42
Oleg Vashchan <guker168@gmail.com>
2021-11-05 19:49:53 +0300

    Раздел 9 Grid Сделали верстку layout начальную

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -8,22 +8,22 @@ import { Footer } from './Footer/Footer';
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
-    <>
-      <Header /> 
-      <div>
-        <Sidebar />
-        <div>{children}</div>
-      </div>
-      <Footer />
-    </>
+    <div className={styles.wrapper}>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <div className={styles.body}>{children}</div>
+      <Footer className={styles.footer} />
+    </div>
   );
 };
 // Функция высшего порядка принимает на вход компонент и возвращает компонент.
-export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
+export const withLayout = <T extends Record<string, unknown>>(
+  Component: FunctionComponent<T>
+) => {
   return function withLayoutComponent(props: T): JSX.Element {
     return (
       <Layout>
-        <Component {...props}/>
+        <Component {...props} />
       </Layout>
     );
   };
