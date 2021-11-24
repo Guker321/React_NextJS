6c2f16faa9a8609bd32f386a99398cc2a5600699
Oleg Vashchan <guker168@gmail.com>
2021-11-24 11:37:43 +0300

    Добавил и настроил getStaticProps в index.ts Добавил и настроил интерфейсы для них для типизации Создал и направил запрос на бек за props и вывел на страницу(урок 64)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Button, Htag, P, Rating, Tag } from '../components';
-import { Layout, withLayout } from '../layout/Layout';
+import { withLayout } from '../layout/Layout';
+import { GetStaticProps } from 'next';
+import axios from 'axios';
+import { MenuItem } from '../interfaces/menu.interface';
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
   const [rating, setRating] = React.useState<number>(4);
   return (
     <>
@@ -30,8 +33,33 @@ function Home(): JSX.Element {
       <Tag color='ghost'>ghost</Tag>
       <Tag>ghost standart</Tag>
       <Rating rating={rating} isEditable={true} setRating={setRating} />
+      <ul>
+        {menu.map((m) => (
+          <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+        ))}
+      </ul>
     </>
   );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  // Делает запрос на сервер Возвращать будет пропсы
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
+    { firstCategory }
+  ); // Получаем результат запроса
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
